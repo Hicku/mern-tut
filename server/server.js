66aa09aef506deef66b0614da1d28a424ef52c80
unknown <aaron.hickman201@gmail.com>
2024-05-04 23:03:26 +0100

    testing routes

--- server/server.js
+++ server/server.js
@@ -1,1 +1,12 @@
-console.log("Hello World!");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const mongoose = require("mongoose");
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+app.use("/api/goals", require("./routes/goalRoutes"));
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
